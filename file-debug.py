9ca3219408f7df0855a841e2cb9c4292348da56c
nattapong-top <nattapong.top@hotmail.com>
2025-11-08 17:42:17 +0700

    ฝึกทำโจทย์ if ซ้อน หรือ if nested if-elif

--- file-debug.py
+++ file-debug.py
@@ -1,5 +1,22 @@
-import math
-x = float(input(': '))
-y = 2 - x + (3 * x**2  / 7) - (5 * x**3 / 11 ) + math.log10(x)
+import random 
 
-print(y)
+
+def delivery(total_purchase: int, location: str) -> str:
+    
+    if total_purchase > 2000:
+        return f'ยอดสั่งซื้อ {total_purchase} ส่งที่ {location} ส่งฟรี '
+    
+    elif total_purchase <= 2000:
+        if location == 'bkk':
+            return f'ยอดสั่งซื้อ {total_purchase} ส่งที่ {location} ค่าส่ง 50 บาท '
+            
+        else:
+            return f'ยอดสั่งซื้อ {total_purchase} ส่งที่ {location} ค่าส่ง 80 บาท '
+
+def call_delivery():
+
+    for item in range(11):
+        total_purchase = random.randint(1500,2500)
+        location = random.choice(['bkk','upcountry'])
+        print(delivery(total_purchase=total_purchase, location=location))
+call_delivery()
