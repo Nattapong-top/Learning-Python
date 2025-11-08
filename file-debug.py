2397b8f9dcab76b9d2bd64864f6419a63f677ad8
nattapong-top <nattapong.top@hotmail.com>
2025-11-08 11:40:01 +0700

    ฝึกโจทย์ if/elif/else พื้นฐาน

--- file-debug.py
+++ file-debug.py
@@ -1,18 +1,5 @@
-def if_04(a:int, b:int = 0):
-    if a % 2 == 0:
-        b = 2 * a
-        
-        if a > 10:
-            a //= 2
-        
-        else:
-            b += a
+import math
+x = float(input(': '))
+y = 2 - x + (3 * x**2  / 7) - (5 * x**3 / 11 ) + math.log10(x)
 
-        a += b
-    a *= 2
-
-    return a, b
-print('8, 4', if_04(8, 4))
-print('112, 41', if_04(112, 41))
-print('3, 1', if_04(3, 1))
-print('2, 1', if_04(2, 1))
+print(y)
